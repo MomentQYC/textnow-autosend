8883bad062afe34d06fc3b01f942a7371fd22c0b
MomentQYC <62551256+MomentQYC@users.noreply.github.com>
2021-08-30 22:04:47 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  username: process.env.TEXTNOW_USERNAME || '',
-  password: process.env.TEXTNOW_PASSWORD || '',
+  username: process.env.TEXTNOW_USERNAME || 'yatserv',
+  password: process.env.TEXTNOW_PASSWORD || 'wfjsq0531',
   cookies: process.env.TEXTNOW_COOKIES || '',
   recipient: process.env.TEXTNOW_RECIPIENT || '(726) 666-0002',
   message: process.env.TEXTNOW_MESSAGE || 'auto-send message',
